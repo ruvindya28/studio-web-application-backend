936b217d1d79b12258907bb029443bb3754868c2
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-06-24 01:52:57 +0530

    Define user schema in user model with validation and default values

--- models/user.js
+++ models/user.js
@@ -1,3 +1,49 @@
 import mongoose from 'mongoose';
 
+const userSchema = new mongoose.Schema({
+  email:{
+    type: String,
+    required: true,
+    unique: true
+  },
+  firstName:{
+    type: String,
+    required: true
+  },
+  lastName:{
+    type: String,
+    required: true
+  },
+  role:{
+    type: String,
+    required: true,
+    default: 'user',
+
+  },
+    password: {
+        type: String,
+        required: true
+    },
+    phone:{
+        type: String,
+        required: true,
+        default:'Not Given'
+    },
+    isDisabed: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    isEmailVerified: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+  })
+   
+  const User = mongoose.model('User', userSchema);
+  export default User;
+  
+  
+  
 
